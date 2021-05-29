c2c3702e2ac35d5c03c276bdd31549d1e2ceb263
kyejin0412 <kyejin0412@kookmin.ac.kr>
2021-05-29 18:09:32 +0900

    base.html

--- day_to_year/day_to_year/settings.py
+++ day_to_year/day_to_year/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'day_to_year.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['day_to_year/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
