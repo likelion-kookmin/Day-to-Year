cad0a518fdb56860720c11b399aca68b950913de
clark1015 <seok626898@gmail.com>
2021-05-29 18:21:02 +0900

    problems solved

--- day_to_year/accounts/urls.py
+++ day_to_year/accounts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('login/', login_view, name="login"),
     path('logout/', logout_view, name="logout"),
     path('signup/', register_view, name="signup"),
+    path('finduser/', find_user, name="find_user")
 ] 
