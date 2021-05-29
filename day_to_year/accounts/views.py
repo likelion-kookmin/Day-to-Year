bf39fb8c1a33a4e9c492b31f3fe0424087cabf28
clark1015 <seok626898@gmail.com>
2021-05-29 14:09:11 +0900

    login page and signup page update

--- day_to_year/accounts/views.py
+++ day_to_year/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterForm
 
 
 # Create your views here.
@@ -13,7 +14,7 @@ def login_view(request):
             user = authenticate(request=request, username=username, password=password)
             if user is None:
                 login(request, user)
-            return redirect("home")
+        return redirect("home")
     else:
         form = AuthenticationForm()
         return render(request, 'login.html', {'form':form})
@@ -21,4 +22,15 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect("home")
+
+def register_view(request):
+    if request.method == "POSt":
+        form = RegisterForm(request.POST)
+        if form.is_valid:
+            user = form.save()
+            login(request, user)
+        return redirect("home")
+    else:
+        form = RegisterForm()
+        return render(request, 'signup.html', {'from': form})
 
--- day_to_year/day_to_year/settings.py
+++ day_to_year/day_to_year/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'accounts.User'
+
 
 # Application definition
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
     
 ]
 
