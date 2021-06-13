ff14357a3739edea83436d046715c81bc0d35bc3
cottonstate <cottonstate3701@kookmin.ac.kr>
2021-06-13 18:24:56 +0900

    rental page

--- day_to_year/rental/urls.py
+++ day_to_year/rental/urls.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .views import *
 from django.urls import path
 urlpatterns = [
-    path('rentallist/', rental , name="rentallist")
+    path('rentallist/', rental , name="rentallist"),
+    path('product/<str:rental_id>', product , name="product"),
+    path('new/', new, name="new"),
+    path('submit/',submit, name="submit")
 ] 
