dc2fa0019cd62d8328ddcebb8bf9b866c1080857
kyejin0412 <kyejin0412@kookmin.ac.kr>
2021-06-27 02:38:35 +0900

    버튼 추가

--- day_to_year/rental/urls.py
+++ day_to_year/rental/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('edit/<str:rental_id>',edit, name ="edit"),
     path('update/<str:rental_id>',update, name="update"),
     path('delete/<str:rental_id>',delete, name="delete"),
-    path('search', search, name='search'),
+    path('search', search, name="search"),
 ] 
 
