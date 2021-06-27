4c4ab023a8ebe5ff8bed1341c2821125f288bbbc
clark1015 <seok626898@gmail.com>
2021-06-27 15:38:56 +0900

    access denied if user is authenticated

--- day_to_year/accounts/views.py
+++ day_to_year/accounts/views.py
@@ -13,26 +13,29 @@ User = get_user_model()
 # Create your views here.
 def login_view(request):
     error=""
-    if request.method =='POST':
-        form = AuthenticationForm(request = request, data= request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get("username")
-            password = form.cleaned_data.get("password")
-            user = authenticate(request = request, username = username, password = password)
-            print("user",user, type(user))
-            if user is None:
-                print('ddd')
-                error = "오류입니다"
-            else:
-                
-                login(request, user)
-                return redirect('main')
-        else:
-            error = "가입하지 않은 아이디이거나, 잘못된 비밀번호입니다"
+    if request.user.is_authenticated:
+        return redirect('main')
+    else:
+        if request.method =='POST':
+            form = AuthenticationForm(request = request, data= request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get("username")
+                password = form.cleaned_data.get("password")
+                user = authenticate(request = request, username = username, password = password)
+                print("user",user, type(user))
+                if user is None:
+                    print('ddd')
+                    error = "오류입니다"
+                else:
 
-    form = AuthenticationForm()
-    print("dsafdsf")
-    return render(request, 'login.html', {'form': form, 'error' : error})
+                    login(request, user)
+                    return redirect('main')
+            else:
+                error = "가입하지 않은 아이디이거나, 잘못된 비밀번호입니다"
+
+        form = AuthenticationForm()
+        print("dsafdsf")
+        return render(request, 'login.html', {'form': form, 'error' : error})
     
 
 
@@ -42,32 +45,40 @@ def logout_view(request):
     return redirect("login")
 
 def register_view(request):
-    if request.method == "POST":
-        form = RegisterForm(request.POST, request.FILES) #
-        if form.is_valid(): #
-            print("ddddd")
-            user = form.save() #git 
-            login(request, user) #
-        return redirect("login")
+    if request.user.is_authenticated:
+        return redirect('main')
+
     else:
-        form = RegisterForm()
-        return render(request, 'signup.html', {'form': form})
+        if request.method == "POST":
+            form = RegisterForm(request.POST, request.FILES) #
+            if form.is_valid(): #
+                print("ddddd")
+                user = form.save() #git 
+                login(request, user) #
+            return redirect("login")
+        else:
+            form = RegisterForm()
+            return render(request, 'signup.html', {'form': form})
 
 def change_password(request):
-    if request.method == 'POST':
-        form = PasswordChangeForm(request.user, request.POST)
-        if form.is_valid():
-            user = form.save()
-            update_session_auth_hash(request, user)
-            messages.success(request, 'Your password was successfully updated!')
-            return redirect('main')
-        else:
-            messages.error(request, 'please correct the error below')
-    
+    if not request.user.is_authenticated:
+        return redirect('main')
     else:
-        form = PasswordChangeForm(request.user)
-    
-    return render(request, 'change_password.html', {'form':form})
+
+        if request.method == 'POST':
+            form = PasswordChangeForm(request.user, request.POST)
+            if form.is_valid():
+                user = form.save()
+                update_session_auth_hash(request, user)
+                messages.success(request, 'Your password was successfully updated!')
+                return redirect('main')
+            else:
+                messages.error(request, 'please correct the error below')
+
+        else:
+            form = PasswordChangeForm(request.user)
+
+        return render(request, 'change_password.html', {'form':form})
 
 def myaccount(request) :
     return render(request, 'account_main.html')
